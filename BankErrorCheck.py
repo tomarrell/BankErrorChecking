88daaa105709f574c82a3b9452ed4ad81dc0e7a3
Tom Arrell <tom.arrell@gmail.com>
2014-07-03 17:01:24 +1200

    See release.txt for changes

--- BankErrorCheck.py
+++ BankErrorCheck.py
@@ -1,15 +1,31 @@
-import os, ConfigParser, time, argparse, paramiko
+import os, ConfigParser, time, argparse, paramiko, getpass
 
-version = "BankErrorCheck v0.6"
+version = "BankErrorCheck v1.1"
 
-config = ConfigParser.ConfigParser()
-config.read('config.cfg')
+config = ConfigParser.RawConfigParser()
+config.read("config.cfg")
+
+save = ""
+
+if config.get("ssh", "host") == "":
+	hostInput = raw_input("There is no SSH host in the config.cfg file. Please enter the SSH host: ")
+	userInput = raw_input("Please enter the username: ")
+	passwordInput = getpass.getpass("Please enter the password: ")
+	save = raw_input("Would you like to save these details to config.cfg? ( y / n ) ")
+	if save == "y" or save == "Y":
+		config.set("ssh", "host", hostInput)
+		config.set("ssh", "username", userInput)
+		config.set("ssh", "password", passwordInput)
+		with open('config.cfg', 'wb') as configfile:
+    			config.write(configfile)
+
 
 ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(
-    paramiko.AutoAddPolicy())
-ssh.connect(config.get("ssh", "host"), username=config.get("ssh", "username"), 
-    password=config.get("ssh", "password"))
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+if config.get("ssh", "host") == "" and (save != "y" or save != "Y"):
+	ssh.connect(hostInput, username=userInput, password=passwordInput)
+else:
+	ssh.connect(config.get("ssh", "host"), username=config.get("ssh", "username"), password=config.get("ssh", "password"))
 print("Connected")
 ftp = ssh.open_sftp()
 ftp.chdir("/")
@@ -18,7 +34,8 @@ parser = argparse.ArgumentParser(description='Collect the date of reports to che
 parser.add_argument("date", help="Enter the 7 digit date in format of YYYYMMDD")
 args = parser.parse_args()
 
-rootReports = config.get("directories", "root_reports")
+rootReportsSend = config.get("directories", "root_reports_send")
+rootReportsReceive = config.get("directories", "root_reports_receive")
 rootSend = config.get("directories", "send_report_directory")
 rootReceive = config.get("directories", "receive_report_directory")
 
@@ -56,14 +73,17 @@ def matchingDirs(directory, listToSave):
 			else:
 				print("Not equal to entered date")
 
-def getFileFromDir(directory, listToSave, subfolder):
+def getFileFromDir(directory, listToSave, subfolder, rootReport):
 	for i in directory:
-		folder = rootReports + "/" + subfolder + "/" + i
+		folder = rootReport + "/" + subfolder + "/" + i
 		ftp.chdir(folder)
 		for files in ftp.listdir(folder):
 			listToSave.append(files)
 
 def comparison(directory, listToSave):
+	for i in mappedFilesSent:
+		i = i[0:-4]
+		return i
 	if directory == mappedFilesSent:
 		against = mappedFilesReceived
 		againstHumanReadable = "received"
@@ -96,10 +116,11 @@ print(matchedDirsReceived)
 
 print("\n/=======/\n")
 
-getFileFromDir(matchedDirsSent, mappedFilesSent, "send")
-getFileFromDir(matchedDirsReceived, mappedFilesReceived, "receive")
+getFileFromDir(matchedDirsSent, mappedFilesSent, config.get("directories", "directory_name_send"), rootReportsSend)
+getFileFromDir(matchedDirsReceived, mappedFilesReceived, config.get("directories", "directory_name_receive"), rootReportsReceive)
 
 print("Files in 'sent' folder:", mappedFilesSent)
+print("\n")
 print("Files in 'received' folder:", mappedFilesReceived)
 
 print("\n/=== DIFFERENCES ===/\n")
@@ -107,22 +128,38 @@ print("\n/=== DIFFERENCES ===/\n")
 comparison(mappedFilesReceived, filesInSentThatDontMatch)
 comparison(mappedFilesSent, filesInReceivedThatDontMatch)
 
+
+
 newFile = open("check_" + time.strftime("%d_%m_%y") + "_for_" + date + '.txt', 'w+')
-newFile.write(version + "\n\n")
+newFile.write(version + "\n")
 newFile.write("Comparison run: " + time.strftime("%c"))
 newFile.write("\n\nDate checked: " + date)
-newFile.write("\n\nThe output compares the two directories (as specified in config.cfg):\n\n" + rootSend + "\nand\n" + rootReceive + "\n\n")
-newFile.write("/==================================/\n\n")
-newFile.write("The files present in the send folder that are not present within the receive foler:\n")
-for i in filesInSentThatDontMatch:
-	newFile.write(i)
-	newFile.write("\n")
+newFile.write("\n\nThe output compares the two directories (as specified in config.cfg):\n\n" + "SEND = " + rootSend + "\n" + "RECEIVE = " +  rootReceive + "\n\n")
+newFile.write("/===================ALL REPORTS==================/\n\n")
+newFile.write("Send Reports: %s\n" % (len(mappedFilesSent)))
+for i in mappedFilesSent:
+	newFile.write(i + "\n")
+newFile.write("\n")
+newFile.write("Received reports: %s\n" % (len(mappedFilesReceived)))
+for i in mappedFilesReceived:
+	newFile.write(i + "\n")
+newFile.write("\n/=================MISSING REPORTS================/\n\n")
+newFile.write("Sent reports with missing received report: %s\n" % (len(filesInReceivedThatDontMatch)))
+if filesInReceivedThatDontMatch == []:
+	newFile.write("No missing received reports\n")
+else:
+	for i in filesInReceivedThatDontMatch:
+		newFile.write(i + " ===> ??")
+		newFile.write("\n")
 
 if config.getboolean("directories", "show_ghost_reports") == True:
-	newFile.write("\n\nThe files present in the received folder that are not present within the send foler:\n")
-	for i in filesInReceivedThatDontMatch:
-		newFile.write(i)
-		newFile.write("\n")
+	newFile.write("\n\nReceived report with missing sent report: %s\n" % (len(filesInSentThatDontMatch)))
+	if filesInSentThatDontMatch == []:
+		newFile.write("No missing sent reports\n")
+	else:
+		for i in filesInSentThatDontMatch:
+			newFile.write("?? <=== " + i)
+			newFile.write("\n")
 
 
 ################### Incorporation of errorCheck.py ###################
@@ -132,21 +169,21 @@ filesUnsuccessful = []
 
 def openCheck(directoryList, type):
 	for i in directoryList:
-		print(rootReports + "/" + type + "/" + i)
-		homeDir = rootReports + "/" + type + "/" + i
+		homeDir = rootReportsReceive + "/" + type + "/" + i
 		ftp.chdir(homeDir)
 		for files in ftp.listdir():
 			files = files.encode('utf-8')
 			path = homeDir + "/" + files
-			print(path)
 			openReport = ftp.open(path, "r", bufsize="1")
 			fileContents = openReport.read()
 			if "Errors" in fileContents:
 				filesWithError.append(path)
+			if "Failed" in fileContents:
+				filesWithError.append(path)
 			if "unsuccessful" in fileContents:
 				filesUnsuccessful.append(path)
 
-openCheck(matchedDirsReceived, "receive")
+openCheck(matchedDirsReceived, config.get("directories", "directory_name_receive"))
 
 print("\n/=== ERRORS ===/\n")
 
@@ -160,9 +197,9 @@ else:
 	print(filesUnsuccessful, "report processed unsuccessfully.")
 
 
-newFile.write("\n/==================================/\n\n")
+newFile.write("\n/=====================ERRORS=====================/\n\n")
 
-newFile.write("Reports processed successfully with error:\n")
+newFile.write("Reports processed successfully with error: %s\n" % (len(filesWithError)))
 
 if filesWithError == []:
 	newFile.write("No errors")
@@ -171,7 +208,7 @@ else:
 		newFile.write(i)
 		newFile.write("\n")
 
-newFile.write("\nReports processed unsuccessfully:\n")
+newFile.write("\nReports processed unsuccessfully: %s\n" % (len(filesUnsuccessful)))
 
 if filesUnsuccessful == []:
 	newFile.write("No unsuccessful files")
---
+++ README.txt
@@ -0,0 +1,13 @@
+Created by Tom Arrell
+See Release.txt for versioning details.
+
+HOWTO:
+
+Edit path and SSH details within config.cfg.
+
+This program requires execution through a command prompt as it takes mandatory arguments in the form of parameters. 
+In order to run the program, within cmd, navigate to the containing directory and run the following command.
+
+>> BankErrorCheck [DATE TO CHECK]
+
+The [DATE TO CHECK] parameter must be included otherwise the program will produce an error. 
